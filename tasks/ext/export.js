d8e96e2005fbaed5b6c1a98674103ff5576d4276
Lloyd Wheeler <lloydwheeler@gmail.com>
2014-09-29 14:51:08 +0100

    create task for generating static builds

---
+++ tasks/ext/export.js
@@ -0,0 +1,44 @@
+module.exports = {
+  name: 'task-export',
+  dependencies: ['task-copy', 'task-prototype'],
+  register: register,
+  buildTask: false
+};
+
+
+
+function register(grunt) {
+
+  // Copy each of our assets folders to new export directory
+  grunt.config('copy.exportCss', {
+    files: [
+      { expand: true, cwd: '<%= config.css_folder %>', src: '**/*.css', dest: 'export/assets/css' }
+    ]
+  });
+
+  grunt.config('copy.exportJs', {
+    files: [
+      { expand: true, cwd: '<%= config.js_folder %>', src: '**/*.js', dest: 'export/assets/js' }
+    ]
+  });
+
+  grunt.config('copy.exportImg', {
+    files: [
+      { expand: true, cwd: '<%= config.img_folder %>', src: '**/*', dest: 'export/assets/img' }
+    ]
+  });
+
+
+
+
+  // Copy our HTML pages to the root of the export directory 
+  grunt.config('copy.exportTemplates', {
+    files: [
+      { expand: true, cwd: '.tmp/html', src: '*.html', dest: 'export/' }
+    ]
+  });
+
+  // Assign task
+  grunt.registerTask('task-export', 'Generates static site from prototype build', ['task-prototype-build', 'copy:exportCss', 'copy:exportJs', 'copy:exportImg', 'copy:exportTemplates']);
+  return true;
+};
