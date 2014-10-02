d6b0194c63079315bb4e4551dc0c49383ca32a73
Lloyd Wheeler <lloydwheeler@gmail.com>
2014-10-02 16:31:01 +0100

    add autoprefixer task

--- setup/assets/cycleconfig.js
+++ setup/assets/cycleconfig.js
@@ -1,7 +1,4 @@
 module.exports = {
-	// Execute production tasks (javascript, styles, prototyping) in parallel. Experimental.
-	"parallel": false,
-	
 	// When production is disabled minification isn't performed.
 	"production": true,
 
@@ -13,32 +10,35 @@ module.exports = {
 		"root": "./",			// The path to our static HTML files
 		"port": 3000,			// The port to run the webserver on
 		"livereload": true,		// Whether to use livereload to refresh content
-		"templates": ""			// The template engine to use, blank for none.
+		"templates": "jade"			// The template engine to use, blank for none.
 
 		// See https://github.com/visionmedia/consolidate.js/#supported-template-engines for supported template engine list.
 		// Any template engines need to be installed globally, i.e. 'npm install hamljs -g', 'npm install jade -g'
 	},
 
 	// Automatically check out files that are under TFS source control and need to be written to 
-	"useTFS": true,
+	"useTFS": false,
 
 	// Folder paths to relevant assets.
 	"css_folder": "assets/css",
 	"js_folder": "assets/js",
 	"scss_folder": "assets/scss",			// Remove me to disable sass
 	"coffee_folder": "assets/coffee",		// Remove me to disable coffeescript
-	
 	"img_folder": "assets/img",
 	//"icon_folder": "assets/img/icons",	// Automatically adds icons in this folder to the stylesheet
 	
 	// If you want to use ruby sass (albeit slower) for compatibility reasons, then enable this option.
 	"use_rubysass": false,
 
+	// To autoprefix, provide autoprefixer options or comment out to disable.
+	"autoprefixer": {
+		"options": {
+			"browsers": ['last 10 versions', 'ie 8', 'ie 9']
+		},
+	},
+	
 	// Are we using requirejs to build our js output?
-	"use_requirejs": false, 
-
-	// Should we generate a source map through our requirejs compilation?
-	"generate_require_sourcemap": false,
+	"use_requirejs": true, 
 
 	// The name of the concatenated app file to generate. 
 	//'app' will generate app.js and app.min.js in production.
@@ -50,29 +50,28 @@ module.exports = {
 
 	// The path to the sitecore project for syncing changes.
 	// If this is removed then no syncing will be performed.
-	"sitecore_path" : "C:/Sitecore/HelloWorld/Website",
+	// "sitecore_path" : "C:/Sitecore/HelloWorld/Website",
 
 	// Set to false to avoid using bower altogether. This will speed up cycle a decent amount.
 	// Bower will copy all your required .css and .js files into vendor.css and vendor.js.
 	// Your bower.json must be present in your project root (the directory you run cycle from).
 	"bower_enabled": true,
 
-	// Automatically merge bower css into the top of our own css file
-	"bower_merge_css": "style.css",
-
 	// Use this to override cycle if it isn't copying the particular 
 	// bower components that you require.
 	"bower_files": {
 		// Example
-		/*"modernizr": {
+		"owl-carousel2": {
 			files: [ 
-				"modernizr.js" 
+				"dist/owl.carousel.js" 
 			]
-		}*/
+		}
 	},
 
 	// Use this to specific bower components which should be included before anything else.
 	"bower_priorities": [
-		"jquery.js"
+		"almond.js",	
+		"jquery.js",
+		"waypoints.js"
 	]
 };
